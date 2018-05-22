8639fab7d2f7895ee918a01ee02def47af825072
ericping1 <eric.ping09@gmail.com>
2018-05-22 14:52:33 -0700

    Fixed Compile Errors

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Layout from './components/Layout'
+
+window.onload = () => {
+  ReactDOM.render(<Layout />, document.getElementById('bottom'));
+};
